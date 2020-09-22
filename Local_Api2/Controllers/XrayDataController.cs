2eaed5ede41a4f62120c6be1fc574c0df5ce8462
Robert Roszak <robert.roszak@outlook.com>
2020-09-22 15:26:32 +0200

    Data fetched from XRay are working

---
+++ Local_Api2/Controllers/XrayDataController.cs
@@ -0,0 +1,69 @@
+﻿using Local_Api2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Local_Api2.Controllers
+{
+    public class XrayDataController : ApiController
+    {
+        [HttpGet]
+        [Route("GetRecentXrayRecords")]
+        [ResponseType(typeof(List<XRayDataRecord>))]
+
+        public IHttpActionResult GetRecentXrayRecords(string MachineName=null)
+        {
+            try
+            {
+                List<XRayDataRecord> Records = new List<XRayDataRecord>();
+                using(SqlConnection XrayConn = new SqlConnection(Static.Secrets.xRayConnectionString))
+                {
+                    
+                    string sql = @"SELECT ArticleName, DeviceName, ProductionStart, ProductionEnd, TimeStamp, Throughput, CounterError, CounterTrade, CounterTotal, CounterBad, CounterContaminated
+                                    FROM StatisticView WHERE ProductionStart >= @StartDate";
+                    if (!string.IsNullOrEmpty(MachineName))
+                    {
+                        sql += $" AND DeviceName LIKE '%{int.Parse(MachineName.Substring(MachineName.Length - 2, 2))}'";
+                    }
+                    sql += " ORDER BY TimeStamp DESC";
+                    SqlCommand command = new SqlCommand(sql, XrayConn);
+                    command.Parameters.Add("@StartDate", SqlDbType.DateTime);
+                    command.Parameters["@StartDate"].Value = DateTime.Now.AddDays(-1);
+                    XrayConn.Open();
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            XRayDataRecord x = new XRayDataRecord();
+                            x.ZfinIndex = Convert.ToInt32(reader["ArticleName"].ToString());
+                            x.DeviceName = reader["DeviceName"].ToString();
+                            x.ProductionStart = reader.GetDateTime(reader.GetOrdinal("ProductionStart"));
+                            x.ProductionEnd = reader.GetDateTime(reader.GetOrdinal("ProductionEnd"));
+                            x.TimeStamp = reader.GetDateTime(reader.GetOrdinal("TimeStamp"));
+                            x.Throughput = Convert.ToInt32(reader["Throughput"].ToString());
+                            x.CounterTrade = Convert.ToInt32(reader["CounterError"].ToString());
+                            x.CounterTotal = Convert.ToInt32(reader["CounterTrade"].ToString());
+                            x.CounterError = Convert.ToInt32(reader["CounterTotal"].ToString());
+                            x.CounterBad = Convert.ToInt32(reader["CounterBad"].ToString());
+                            x.CounterContaminated = Convert.ToInt32(reader["CounterContaminated"].ToString());
+                            Records.Add(x);
+                        }
+                    }
+                    return Ok(Records);
+                }
+
+                
+            }catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+            
+        }
+    }
+}
---
+++ Local_Api2/Models/XRayDataRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Local_Api2.Models
+{
+    public class XRayDataRecord
+    {
+        public int ZfinIndex { get; set; }
+        public string DeviceName { get; set; }
+        public DateTime? ProductionStart { get; set; }
+        public DateTime? ProductionEnd { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int Throughput { get; set; }
+        public int CounterError { get; set; }
+        public int CounterTrade { get; set; }
+        public int CounterTotal { get; set; }
+        public int CounterBad { get; set; }
+        public int CounterContaminated { get; set; }
+
+
+    }
+}
